c8f8a100070349292bc1e680356602df41b8d305
nickdepaolo <79952881+nickdepaolo@users.noreply.github.com>
2021-05-08 21:21:19 -0400

    9:30 5/8

--- src/Components/PokeFetch/PokeFetch.js
+++ src/Components/PokeFetch/PokeFetch.js
@@ -9,6 +9,7 @@ class PokeFetch extends Component {
       pokeInfo: '',
       pokeSprite: '',
       pokeName: '',
+      timer: 11
     }
   }
 
@@ -16,18 +17,47 @@ class PokeFetch extends Component {
     let min = Math.ceil(1);
     let max = Math.floor(152);
     let pokeNum = Math.floor(Math.random() * (max - min) + min);
+
+    this.changeState();
+    
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNum}`, {
       method: 'GET'
     }).then(res => res.json())
-      .then(res => {
-        this.setState({
-          pokeInfo: res,
-          pokeSprite: res.sprites.front_default,
-          pokeName: res.species.name,
-        })
+    .then(res => {
+      this.setState({
+        pokeInfo: res,
+        pokeSprite: res.sprites.front_default,
+        pokeName: res.species.name,
+      })
+      this.setState({
+        timer: 11
+      })
+      
+      this.decrement();
       })
       .catch((err) => console.log(err))
   }
+
+  changeState() {
+    this.setState({
+      pokeSprite: '',
+      pokeName: '',
+      timer: 11
+    })
+  }
+
+  count() {
+    this.setState({
+      timer: (this.state.timer - 1)
+    });
+    setTimeout(() => this.decrement(), 1000)
+  };
+
+  decrement() {
+    if (this.state.timer > 0) {
+      this.count()
+    }
+  };
 
   render() {
     return (
@@ -35,8 +65,11 @@ class PokeFetch extends Component {
         <button className={'start'} onClick={() => this.fetchPokemon()}>Start!</button>
         <h1 className={'timer'} >Timer Display</h1>
         <div className={'pokeWrap'}>
-          <img className={'pokeImg'} src={this.state.pokeSprite} />
-          <h1 className={'pokeName'}>{this.state.pokeName}</h1>
+          {this.state.pokeSprite? <img className={'pokeImg'}  src={this.state.pokeSprite} /> : ''}
+          <div>
+             {this.state.pokeName? <h1 className={'pokeName'}>{this.state.pokeName}</h1> : ''}
+          </div>
+          <div>{this.state.timer < 11 && this.state.timer > 0 ? <h1>{this.state.timer}</h1> : ''}</div>
         </div>
       </div>
     )
